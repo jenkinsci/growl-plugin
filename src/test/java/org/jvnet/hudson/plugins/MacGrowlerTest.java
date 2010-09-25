d29e6456cc0d0b75f3b31dc3f3b614ada45ff59c
srb55 <srb55@dev.java.net>
2010-09-25 22:51:13 +0000

    imported growl plugin

---
+++ src/test/java/org/jvnet/hudson/plugins/MacGrowlerTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.jvnet.hudson.plugins;
+
+
+import org.junit.Before;
+
+/**
+ * @author srb55
+ *
+ */
+public class MacGrowlerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	public void test() throws Exception {
+	}
+}
