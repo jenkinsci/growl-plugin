d82175e1c9eb9deb01b82cc1cb897f65d383ee6a
William Kina <thekina@gmail.com>
2011-05-07 03:20:35 -0300

    changed space to tab

--- src/main/java/hudson/plugins/growl/GrowlPublisher.java
+++ src/main/java/hudson/plugins/growl/GrowlPublisher.java
@@ -42,54 +42,54 @@ import hudson.plugins.growl.MacGrowler;
 @SuppressWarnings("unchecked")
 public class GrowlPublisher extends Notifier {
 
-    private static final List<String> VALUES_REPLACED_WITH_NULL = Arrays.asList("", "(Default)",
-            "(System Default)");
+		private static final List<String> VALUES_REPLACED_WITH_NULL = Arrays.asList("", "(Default)",
+						"(System Default)");
 
-    private static final Logger LOGGER = Logger.getLogger(GrowlPublisher.class.getName());
-    private final String appName = "Growler";
-    
-    private String IP;
-    private Boolean onlyOnFailureOrRecovery;
+		private static final Logger LOGGER = Logger.getLogger(GrowlPublisher.class.getName());
+		private final String appName = "Growler";
+		
+		private String IP;
+		private Boolean onlyOnFailureOrRecovery;
 
-    private GrowlPublisher(String IP, Boolean onlyOnFailureOrRecovery) {
-        this.onlyOnFailureOrRecovery = onlyOnFailureOrRecovery;
-        this.IP = IP;
-    }
+		private GrowlPublisher(String IP, Boolean onlyOnFailureOrRecovery) {
+				this.onlyOnFailureOrRecovery = onlyOnFailureOrRecovery;
+				this.IP = IP;
+		}
 
-    @DataBoundConstructor
-    public GrowlPublisher(String IP,String onlyOnFailureOrRecovery) {
-        this(cleanToString(IP), cleanToBoolean(onlyOnFailureOrRecovery));
-    }
+		@DataBoundConstructor
+		public GrowlPublisher(String IP,String onlyOnFailureOrRecovery) {
+				this(cleanToString(IP), cleanToBoolean(onlyOnFailureOrRecovery));
+		}
 
-    private static String cleanToString(String string) {
-        return VALUES_REPLACED_WITH_NULL.contains(string) ? null : string;
-    }
+		private static String cleanToString(String string) {
+				return VALUES_REPLACED_WITH_NULL.contains(string) ? null : string;
+		}
 
-    private static Boolean cleanToBoolean(String string) {
-    	Boolean result = null;
-    	if ("true".equals(string) || "Yes".equals(string)) {
-    		result = Boolean.TRUE;
-    	} else if ("false".equals(string) || "No".equals(string)) {
-    		result = Boolean.FALSE;
-    	}
-    	return result;
-    }
+		private static Boolean cleanToBoolean(String string) {
+			Boolean result = null;
+			if ("true".equals(string) || "Yes".equals(string)) {
+				result = Boolean.TRUE;
+			} else if ("false".equals(string) || "No".equals(string)) {
+				result = Boolean.FALSE;
+			}
+			return result;
+		}
 
-   private static String createTinyUrl(String url) throws IOException {
-        HttpClient client = new HttpClient();
-        GetMethod gm = new GetMethod("http://tinyurl.com/api-create.php?url="
-                + url.replace(" ", "%20"));
+	 private static String createTinyUrl(String url) throws IOException {
+				HttpClient client = new HttpClient();
+				GetMethod gm = new GetMethod("http://tinyurl.com/api-create.php?url="
+								+ url.replace(" ", "%20"));
 
-        int status = client.executeMethod(gm);
-        if (status == HttpStatus.SC_OK) {
-            return gm.getResponseBodyAsString();
-        } else {
-            throw new IOException("Non-OK response code back from tinyurl: " + status);
-        }
+				int status = client.executeMethod(gm);
+				if (status == HttpStatus.SC_OK) {
+						return gm.getResponseBodyAsString();
+				} else {
+						throw new IOException("Non-OK response code back from tinyurl: " + status);
+				}
 
-    }
-   
-   private String createGrowlMessage(AbstractBuild<?, ?> build) {
+		}
+	 
+	 private String createGrowlMessage(AbstractBuild<?, ?> build) {
 		String projectName = build.getProject().getName();
 		String result = build.getResult().toString();
 	
@@ -105,51 +105,51 @@ public class GrowlPublisher extends Notifier {
 		return String.format("Project: %s\nStatus: %s\nBuild Number: %d\nURL:%s", projectName, result, build.number, tinyUrl);
 	}
 
-    public static boolean fallbackPingHost(String host) {
-        try
-        {
-            String command = "ping " + (System.getProperty("os.name").toLowerCase().indexOf("win")>=0) ? "-n 1 -w 1000 " : "-c 1 -W 1 ";
-            Process p = Runtime.getRuntime().exec(command + host);
-            p.waitFor();
-            return (p.exitValue() == 0);
-        } catch (Exception e) {
-            return false;
-        }
-    }
+		public static boolean fallbackPingHost(String host) {
+				try
+				{
+						String command = "ping " + (System.getProperty("os.name").toLowerCase().indexOf("win")>=0) ? "-n 1 -w 1000 " : "-c 1 -W 1 ";
+						Process p = Runtime.getRuntime().exec(command + host);
+						p.waitFor();
+						return (p.exitValue() == 0);
+				} catch (Exception e) {
+						return false;
+				}
+		}
 
 	private boolean pingHost(String host) {
 		try
 		{
-		    InetAddress address = InetAddress.getByName(host);
-		    return (address.isReachable(10000) || fallbackPingHost(host));
+				InetAddress address = InetAddress.getByName(host);
+				return (address.isReachable(10000) || fallbackPingHost(host));
 		} catch (Exception e)
 		{
-		    e.printStackTrace();
-		    return false;
+				e.printStackTrace();
+				return false;
 		}
 	}
 
-    public String getIP() {
-        return IP;
-    }
+		public String getIP() {
+				return IP;
+		}
 
-    public Boolean getOnlyOnFailureOrRecovery() {
-        return onlyOnFailureOrRecovery;
-    }
+		public Boolean getOnlyOnFailureOrRecovery() {
+				return onlyOnFailureOrRecovery;
+		}
 
-    public BuildStepMonitor getRequiredMonitorService() {
-        return BuildStepMonitor.BUILD;
-    }
+		public BuildStepMonitor getRequiredMonitorService() {
+				return BuildStepMonitor.BUILD;
+		}
 
-    @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
-    	if (shouldgrowl(build)) {
-    		String password = ((DescriptorImpl) getDescriptor()).password;
-            try {
-            	String [] clients = IP.replace(" ","").split(",");
-            	for (int i=0; i< clients.length; i++) {
-            		if (!pingHost(clients[i])) {
-            			LOGGER.log(Level.INFO, "Cannot send  Growl to " + clients[i] + ", host is down.");
+		@Override
+		public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+			if (shouldgrowl(build)) {
+				String password = ((DescriptorImpl) getDescriptor()).password;
+						try {
+							String [] clients = IP.replace(" ","").split(",");
+							for (int i=0; i< clients.length; i++) {
+								if (!pingHost(clients[i])) {
+									LOGGER.log(Level.INFO, "Cannot send	 Growl to " + clients[i] + ", host is down.");
 									return;
 								}
 								LOGGER.log(Level.INFO, "Sending Growl to " + clients[i] + "...");
@@ -173,17 +173,17 @@ public class GrowlPublisher extends Notifier {
 									MacGrowler notifier = MacGrowler.register( appName, password, clients[i]);
 									notifier.notify( appName, "Jenkins Build", message, password);
 								}
-            	}
-            } catch (Exception e) {
-                LOGGER.log(Level.SEVERE, "Unable to send growl.", e);
-                return false;
-            }
-    	}
-    	
-        return true;
-    }
-    
-    /**
+							}
+						} catch (Exception e) {
+								LOGGER.log(Level.SEVERE, "Unable to send growl.", e);
+								return false;
+						}
+			}
+			
+				return true;
+		}
+		
+		/**
 	 * Determine if this build results should be growled. Uses the local
 	 * settings if they are provided, otherwise the global settings.
 	 *
@@ -203,89 +203,89 @@ public class GrowlPublisher extends Notifier {
 			return true;
 		}
 	}
-    
-    /**
-     * Determine if this build represents a failure or recovery. A build failure
-     * includes both failed and unstable builds. A recovery is defined as a
-     * successful build that follows a build that was not successful. Always
-     * returns false for aborted builds.
-     *
-     * @param build the Build object
-     * @return true if this build represents a recovery or failure
-     */
-    protected boolean isFailureOrRecovery(AbstractBuild<?, ?> build) {
-        if (build.getResult() == Result.FAILURE || build.getResult() == Result.UNSTABLE) {
-            return true;
-        } else if (build.getResult() == Result.SUCCESS) {
-        	AbstractBuild<?, ?> previousBuild = build.getPreviousBuild();
-            if (previousBuild != null && previousBuild.getResult() != Result.SUCCESS) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+		
+		/**
+		 * Determine if this build represents a failure or recovery. A build failure
+		 * includes both failed and unstable builds. A recovery is defined as a
+		 * successful build that follows a build that was not successful. Always
+		 * returns false for aborted builds.
+		 *
+		 * @param build the Build object
+		 * @return true if this build represents a recovery or failure
+		 */
+		protected boolean isFailureOrRecovery(AbstractBuild<?, ?> build) {
+				if (build.getResult() == Result.FAILURE || build.getResult() == Result.UNSTABLE) {
+						return true;
+				} else if (build.getResult() == Result.SUCCESS) {
+					AbstractBuild<?, ?> previousBuild = build.getPreviousBuild();
+						if (previousBuild != null && previousBuild.getResult() != Result.SUCCESS) {
+								return true;
+						} else {
+								return false;
+						}
+				} else {
+						return false;
+				}
+		}
 
-   
-    @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+	 
+		@Extension
+		public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
-        public String password;
-        public boolean onlyOnFailureOrRecovery;
-        public String hudsonUrl;
-        
-        public DescriptorImpl() {
-            super(GrowlPublisher.class);
-            load();
-        }
+				public String password;
+				public boolean onlyOnFailureOrRecovery;
+				public String hudsonUrl;
+				
+				public DescriptorImpl() {
+						super(GrowlPublisher.class);
+						load();
+				}
 
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // set the booleans to false as defaults
-            onlyOnFailureOrRecovery = false;
+				@Override
+				public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+						// set the booleans to false as defaults
+						onlyOnFailureOrRecovery = false;
 
-            req.bindParameters(this, "growl.");
-            hudsonUrl = Mailer.descriptor().getUrl();
-            
-            save();
-            return super.configure(req, formData);
+						req.bindParameters(this, "growl.");
+						hudsonUrl = Mailer.descriptor().getUrl();
+						
+						save();
+						return super.configure(req, formData);
 			//return includeUrl;
-        }
+				}
 
-        @Override
-        public String getDisplayName() {
-            return "Growl";
-        }
+				@Override
+				public String getDisplayName() {
+						return "Growl";
+				}
 
-        public String getPassword() {
-            return password;
-        }
+				public String getPassword() {
+						return password;
+				}
 
-        public String getUrl() {
-            return hudsonUrl;
-        }
-        
-        public boolean isOnlyOnFailureOrRecovery() {
-            return onlyOnFailureOrRecovery;
-        }
+				public String getUrl() {
+						return hudsonUrl;
+				}
+				
+				public boolean isOnlyOnFailureOrRecovery() {
+						return onlyOnFailureOrRecovery;
+				}
 
-        @Override
-        public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
-            return true;
-        }
+				@Override
+				public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
+						return true;
+				}
 
-        @Override
-        public Publisher newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-        	
-            if (hudsonUrl == null) {
-                //if Hudson URL is not configured yet, infer some default
-                hudsonUrl = Functions.inferHudsonURL(req);
-                save();
-            }
-            
-            return super.newInstance(req, formData);
-        }
-    }
+				@Override
+				public Publisher newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+					
+						if (hudsonUrl == null) {
+								//if Hudson URL is not configured yet, infer some default
+								hudsonUrl = Functions.inferHudsonURL(req);
+								save();
+						}
+						
+						return super.newInstance(req, formData);
+				}
+		}
 }
